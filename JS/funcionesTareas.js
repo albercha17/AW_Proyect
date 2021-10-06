be7b0f6230be2d6ff7a6c93ca8caa0fa9278fe17
albercha17 <albercha@ucm.es>
2021-10-06 19:19:07 +0200

    js casi entero

--- JS/funcionesTareas.js
+++ JS/funcionesTareas.js
@@ -1,32 +1,32 @@
 let listaTareas = [
-    {text:"Preparar práctica AW", tags:["AW","practica"]},
+     {text:"Preparar práctica AW", tags:["AW","practica"]},
      {text: "Mirar fechas congreso", done: true, tags:[]}, 
      {text: "Ir al supermercado", tags: ["personal"]}, 
      {text: "Mudanza", done: false, tags:["personal"]}
      ];
 
-     function getToDoTasks(listaTareas){
-        
-        return listaT;    
+function getToDoTasks(tasks){
+        let listaT=listaTareas
+        return listaT;
      }
-     
-      function findByTag(tasks, tag){
-         let listaT=listaTareas.filter(n=>n.tags.filter(m=>m===tag));
-         return listaT;    
-  
+
+function findByTag(tasks, tag){
+         let listaT=tasks.filter(n=>n.tags.includes(tag))
+         return listaT; 
          }
 
-     function findByTags(tasks, tags){
+function findByTags(tasks, tags){
+        let listaT=tasks.filter(n=>n.tags.some(e => tags.includes(e)))
         return listaT;    
 
      }
 
-     function countDone(tasks){
-         let listaT=listaTareas.filter(n=>n.done===true);
+function countDone(tasks){
+         let listaT=tasks.filter(n=>n.done===true)
          var contador=listaT.length
          return contador;
      }
 
-     function createTask(texto){
+function createTask(texto){
 
      }
