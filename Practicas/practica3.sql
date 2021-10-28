888451d05dd359fa2e47a9dd8fe872b2e1ff7954
albercha17 <64268285+albercha17@users.noreply.github.com>
2021-10-28 20:20:57 +0200

    sql

--- Practicas/practica3.sql
+++ Practicas/practica3.sql
@@ -1,9 +1,9 @@
 
-create table usuario(email VARCHAR2(100), password VARCHAR2(100), img VARCHAR2(100),PRIMARY KEY(email)));
+create table usuario(email VARCHAR2(100), password VARCHAR2(100), img VARCHAR2(100),PRIMARY KEY(email));
 insert into usuario('albercha@ucm.es','12345','user_img1.jpg');
 insert into usuario('albercha17@gmail.com','12345','user_img2.jpg');
 
-create table task(id VARCHAR2(11), usuario VARCHAR2(100), text VARCHAR2(100), done VARCHAR2(1),PRIMARY KEY(id)));
+create table task(id VARCHAR2(11), usuario VARCHAR2(100), text VARCHAR2(100), done VARCHAR2(1),PRIMARY KEY(id));
 insert into task values('1','albercha@ucm.es','Ir al gym','0');
 insert into task values('2','albercha17@gmail.es','Ir al gym','1');
 
@@ -17,7 +17,7 @@ insert into tag values('1','tarde');
 select 
     "ID",
     "USUARIO",
-    "TEXY",
+    "TEXT",
     "DONE"
 from "TASK";
 
