76217c2fdf4b41ec5243464347edb937edf66a31
albercha17 <64268285+albercha17@users.noreply.github.com>
2021-11-02 20:39:37 +0100

    sql bien

--- JS/main.js
+++ JS/main.js
@@ -14,3 +14,15 @@ let daoUser = new DAOUsers(pool);
 let daoTask = new DAOTasks(pool);
 // Definición de las funciones callback
 // Uso de los métodos de las clases DAOUsers y DAOTasks
+
+daoUser.isUserCorrect("albercha@ucm.es", "12345", cb_isUserCorrect);
+
+function cb_isUserCorrect(err, result) {
+    if (err) {
+        console.log(err.message);
+    } else if (result) {
+        console.log("Usuario y contraseña correctos");
+    } else {
+        console.log("Usuario y/o contraseña incorrectos");
+    }
+}
--- Practicas/practica3.sql
+++ Practicas/practica3.sql
@@ -1,13 +1,22 @@
 
-create table usuario(email VARCHAR2(100), password VARCHAR2(100), img VARCHAR2(100),PRIMARY KEY(email));
-insert into usuario values('albercha@ucm.es','12345','user_img1.jpg');
-insert into usuario values('albercha17@gmail.com','12345','user_img2.jpg');
+create table user(email VARCHAR(100), password VARCHAR(100), img VARCHAR(100),PRIMARY KEY(email));
+insert into user values('albercha@ucm.es','12345','user_img1.jpg');
+insert into user values('inmap@ucm.es','ucm1','user_img3.jpg');
+insert into user values('albercha17@gmail.com','12345','user_img2.jpg');
+insert into user values('pedro@ucm.es','12345','user_img4.jpg');
 
-create table task(id VARCHAR2(11), usuario VARCHAR2(100), text VARCHAR2(100), done int,PRIMARY KEY(id));
+create table task(id int(11), user VARCHAR(100), text text, done tinyint(1),PRIMARY KEY(id));
 insert into task values('1','albercha@ucm.es','Ir al gym','0');
 insert into task values('2','albercha17@gmail.es','Ir al gym','1');
+insert into task values('4','albercha17@gmail.es','Ir al cine','0');
+insert into task values('3','pedro@ucm.es','Ir al gym','0');
+insert into task values('5','albercha@ucm.es','Ir a la compra','0');
+insert into task values('6','albercha@ucm.es','Ir al cine','1');
+insert into task values('7','albercha@ucm.es','Ir al super');
 
-create table tag(taskId CHAR(11), tag VARCHAR2(100) ,PRIMARY KEY(taskId, tag));
+
+
+create table tag(taskId int(11), tag VARCHAR(100) ,PRIMARY KEY(taskId, tag));
 insert into tag values('1','deporte');
 insert into tag values('1','tarde');
 
