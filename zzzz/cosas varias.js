0c34447a374911cb7bac318fce60212f161ccb7a
albercha17 <64268285+albercha17@users.noreply.github.com>
2021-11-10 17:10:29 +0100

    sql bien

--- zzzz/cosas varias.js
+++ zzzz/cosas varias.js
@@ -57,26 +57,26 @@ function validarEmail(email) {
 
 //-------------------------------------------------------------------------------------------------------------------------------
 //base de datos
-create table user(email VARCHAR(100), password VARCHAR(100),nombre VARCHAR(100), img VARCHAR(100),PRIMARY KEY(email));
-insert into user values('nico@404.es ','1234','Nico','/Imagenes_de_perfil/nico.png');
-insert into user values('roberto@404.es','1234','Roberto','/Imagenes_de_perfil/roberto.png');
-insert into user values('sfg@404.es','1234','SFG','/Imagenes_de_perfil/sfg.png');
-insert into user values('marta@404.es','1234','Marta','/Imagenes_de_perfil/marta.png');
-insert into user values('lucas@404.es','1234','Lucas','/Imagenes_de_perfil/kuroko.png');
-insert into user values('emy@404.es','1234','Emy','/Imagenes_de_perfil/amy.png');
+create table user(email VARCHAR(100), password VARCHAR(100),nombre VARCHAR(100), img VARCHAR(100), reputacion int(11),PRIMARY KEY(email));
+insert into user values('nico@404.es ','1234','Nico','/Imagenes_de_perfil/nico.png','1');
+insert into user values('roberto@404.es','1234','Roberto','/Imagenes_de_perfil/roberto.png','1');
+insert into user values('sfg@404.es','1234','SFG','/Imagenes_de_perfil/sfg.png','1');
+insert into user values('marta@404.es','1234','Marta','/Imagenes_de_perfil/marta.png','1');
+insert into user values('lucas@404.es','1234','Lucas','/Imagenes_de_perfil/kuroko.png','1');
+insert into user values('emy@404.es','1234','Emy','/Imagenes_de_perfil/amy.png','1');
 
-create table pregunta(id int(11), titulo VARCHAR(100), cuerpo text, autor VARCHAR(100),PRIMARY KEY(id));
+create table pregunta(id int(11), titulo VARCHAR(100), cuerpo text, autor VARCHAR(100),puntos int(11),PRIMARY KEY(id));
 insert into pregunta values('1','¿Cual es la diferencia entre position: relative, position: absolute y position: fixed?'
 ,'Sé que estas propiedades de CSS sirven para posicionar un elemento dentro de la página. Séque estas propiedades de CSS sirven para posicionar un elemento dentro de la página.',
-'nico@404.es');
+'nico@404.es','0');
 insert into pregunta values('2','¿Cómo funciona exactamente nth-child?','No acabo de comprender muy bien que hace exactamente y qué usos prácticos puede tener',
-'roberto@404.es');
+'roberto@404.es','0');
 insert into pregunta values('3',' Diferencias entre == y === (comparaciones en JavaScript)','Siempre he visto que en JavaScript hay:asignaciones =comparaciones == y ===Creo entender que == hace algo parecido a comparar el valor de la variable y el === tambiéncompara el tipo (como un equals de java).',
-'sfg@404.es');
+'sfg@404.es','0');
 insert into pregunta values('4','Problema con asincronismo en Node','Soy nueva en Node... Tengo una modulo que conecta a una BD de postgres por medio de pgnode. En eso no tengo problemas. Mi problema es que al llamar a ese modulo, desde otromodulo, y despues querer usar los datos que salieron de la BD me dice undefined... Estoy casiseguro que es porque la conexion a la BD devuelve una promesa, y los datos no estandisponibles al momento de usarlos.',
-'marta@404.es');
+'marta@404.es','0');
 insert into pregunta values('5','¿Qué es la inyección SQL y cómo puedo evitarla?','He encontrado bastantes preguntas en StackOverflow sobre programas o formularios web que guardan información en una base de datos (especialmente en PHP y MySQL) y que contienen graves problemas de seguridad relacionados principalmente con la inyección SQL. Normalmente dejo un comentario y/o un enlace a una referencia externa, pero un comentario no da mucho espacio para mucho y sería positivo que hubiera una referencia interna en SOessobre el tema así que decidí escribir esta pregunta.',
-'lucas@404.es');
+'lucas@404.es','0');
 
 
 create table etiqueta(idPregunta int(11), tag VARCHAR(100) ,PRIMARY KEY(idPregunta, tag));
@@ -101,7 +101,11 @@ insert into respuesta values('2','1','La pseudoclase :nth-child() selecciona los
  // seleccionar preguntas por autor
 SELECT * FROM pregunta T, etiqueta a WHERE T.autor = 'nico@404.es' AND T.id=a.idPregunta;
 
+//Seleccionar todas las preguntas de la BD
+SELECT * FROM pregunta T, etiqueta a WHERE T.id=a.idPregunta;
+
 // si buscamos que contenga una palabra
 SELECT * FROM pregunta T WHERE columna1 LIKE '%'+palbara+'%' AND columnna2 like '%'palabra2+'%';
+
 //ejemplo----> me busca la preguntacon sus etiquetas de aquellas preguntas quw contengan Node o JavaScript
 SELECT * FROM pregunta T , etiqueta a WHERE (titulo LIKE '%Node%' OR titulo like '%JavaScript%') AND T.id=a.idPregunta;
