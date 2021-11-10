1988179ba2fa5fa9813d1af52afcee1e515504c4
albercha17 <64268285+albercha17@users.noreply.github.com>
2021-11-10 17:31:36 +0100

    sql terminado

--- zzzz/cosas varias.js
+++ zzzz/cosas varias.js
@@ -65,18 +65,18 @@ insert into user values('marta@404.es','1234','Marta','/Imagenes_de_perfil/marta
 insert into user values('lucas@404.es','1234','Lucas','/Imagenes_de_perfil/kuroko.png','1');
 insert into user values('emy@404.es','1234','Emy','/Imagenes_de_perfil/amy.png','1');
 
-create table pregunta(id int(11), titulo VARCHAR(100), cuerpo text, autor VARCHAR(100),puntos int(11),PRIMARY KEY(id));
+create table pregunta(id int(11), titulo VARCHAR(100), cuerpo text, autor VARCHAR(100),visitas int(11),puntos int(11),PRIMARY KEY(id));
 insert into pregunta values('1','¿Cual es la diferencia entre position: relative, position: absolute y position: fixed?'
 ,'Sé que estas propiedades de CSS sirven para posicionar un elemento dentro de la página. Séque estas propiedades de CSS sirven para posicionar un elemento dentro de la página.',
-'nico@404.es','1');
+'nico@404.es','1','0');
 insert into pregunta values('2','¿Cómo funciona exactamente nth-child?','No acabo de comprender muy bien que hace exactamente y qué usos prácticos puede tener',
-'roberto@404.es','0');
+'roberto@404.es','0','0');
 insert into pregunta values('3',' Diferencias entre == y === (comparaciones en JavaScript)','Siempre he visto que en JavaScript hay:asignaciones =comparaciones == y ===Creo entender que == hace algo parecido a comparar el valor de la variable y el === tambiéncompara el tipo (como un equals de java).',
-'sfg@404.es','0');
+'sfg@404.es','0','0');
 insert into pregunta values('4','Problema con asincronismo en Node','Soy nueva en Node... Tengo una modulo que conecta a una BD de postgres por medio de pgnode. En eso no tengo problemas. Mi problema es que al llamar a ese modulo, desde otromodulo, y despues querer usar los datos que salieron de la BD me dice undefined... Estoy casiseguro que es porque la conexion a la BD devuelve una promesa, y los datos no estandisponibles al momento de usarlos.',
-'marta@404.es','0');
+'marta@404.es','0','0');
 insert into pregunta values('5','¿Qué es la inyección SQL y cómo puedo evitarla?','He encontrado bastantes preguntas en StackOverflow sobre programas o formularios web que guardan información en una base de datos (especialmente en PHP y MySQL) y que contienen graves problemas de seguridad relacionados principalmente con la inyección SQL. Normalmente dejo un comentario y/o un enlace a una referencia externa, pero un comentario no da mucho espacio para mucho y sería positivo que hubiera una referencia interna en SOessobre el tema así que decidí escribir esta pregunta.',
-'lucas@404.es','0');
+'lucas@404.es','0','0');
 
 
 create table etiqueta(idPregunta int(11), tag VARCHAR(100) ,PRIMARY KEY(idPregunta, tag));
@@ -97,6 +97,9 @@ insert into respuesta values('2','1','La pseudoclase :nth-child() selecciona los
 
 create table puntos(idPregunta int(11), user VARCHAR(100), punto tinyint(1),PRIMARY KEY(idPregunta,user));
 insert into puntos values('1','roberto@404.es','1');
+
+create table medallas(user VARCHAR(100), tipo VARCHAR(100),nombre VARCHAR(100),PRIMARY KEY(nombre,user));
+insert into medallas values('roberto@404.es','bronce','Estudiante');
 
 //-------------------------------------------------------------------------------------------------------------------------------
 // otras operaciones que podrian ayudar en la bd
